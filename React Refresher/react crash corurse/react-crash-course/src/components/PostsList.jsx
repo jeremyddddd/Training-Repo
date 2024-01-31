102dc1147c3b621b6f746a6792cc0e6785347c69
Jeremyfdomingo <jeremyfdomingo@gmail.com>
2024-01-30 19:06:40 -0600

    React refresher complete

--- React Refresher/react crash corurse/react-crash-course/src/components/PostsList.jsx
+++ React Refresher/react crash corurse/react-crash-course/src/components/PostsList.jsx
@@ -1,30 +1,17 @@
-import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 import Post from './Post';
 import classes from './PostsList.module.css';
-import Modal from './Modal';
-import NewPost from './NewPost';
 
-function PostsList({isPosting, onStopPosting}) {
-  const [posts, setPosts] = useState([]);
-
-  function addPostHandler(postData) {
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
-
+function PostsList() {
+const posts = useLoaderData();
     return (
         <>
-          {isPosting && (
-            <Modal onClose={onStopPosting}>
-              <NewPost 
-                onCancel={onStopPosting}
-                onAddPost={addPostHandler}
-              />
-            </Modal>
-          )}
           {posts.length > 0 && (
             <ul className={classes.posts}>
-              {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+              {posts.map((post) => (
+                <Post key={post.id} id={post.id} author={post.author} body={post.body} />
+              ))}
             </ul>
           )}
           {posts.length === 0 && (
@@ -33,7 +20,6 @@ function PostsList({isPosting, onStopPosting}) {
               <p>Start adding some!</p>
             </div>
           )}
-
         </>
       );
 }
